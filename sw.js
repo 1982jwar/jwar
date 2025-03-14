d0b336fb3e4228e19fb24e81db632c6433f8b776
joaowelton <joaowelton2012@gmail.com>
2025-03-14 18:03:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Versão do cache (aumente para atualizar)
-const CACHE_NAME = 'karaoke-cache-v5';
+const CACHE_NAME = 'karaoke-cache-v6';
 
 // Arquivos para cache (atualize com seus caminhos)
 const ASSETS = [
